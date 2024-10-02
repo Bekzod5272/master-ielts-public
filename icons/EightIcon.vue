a38f765516bf03c8e2873f5c0dce8af8653e7c5b
b.mirxolov <b.mirxolov@agrobank.uz>
2024-10-02 11:36:17 +0500

    first commit

---
+++ icons/EightIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xodm="http://www.corel.com/coreldraw/odm/2003" xml:space="preserve" width="18.5664mm" height="22.31mm" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" viewBox="0 0 32.01 38.47">
+ <defs>
+   <linearGradient id="id0" gradientUnits="userSpaceOnUse" x1="15.99" y1="37.14" x2="15.99" y2="1.18">
+   <stop offset="0" style="stop-opacity:1; stop-color:#4F26DA"/>
+     <stop offset="1" style="stop-opacity:1; stop-color:#AF34EE"/>
+  </linearGradient>
+ </defs>
+    <g id="Слой_x0020_1">
+  <metadata id="CorelCorpID_0Corel-Layer"/>
+      <path class="fil0" d="M8.32 27.33c-2.77,0.27 -5.55,0.54 -8.32,0.8 0.29,-3.65 1.89,-6.92 5.95,-9.48 -5.62,-4.34 -6.07,-10.13 -1.91,-14.61 3.14,-3.39 8.73,-4.6 14.63,-3.81 4.93,0.66 11.58,2.8 11.92,10.56 0.16,3.54 -1.84,6.49 -4.84,8.59 4.08,2.2 6.56,5.15 6.22,9.44 -0.32,5.19 -3.51,8.05 -8.25,9.64 -0.24,-2.67 -0.48,-5.33 -0.72,-8 2.14,-2.25 0.14,-5.68 -5.42,-7.25 -2.17,-0.61 -4.22,-1.39 -6.37,-2.06 -2.02,1.79 -2.96,3.86 -2.89,6.17zm14.46 -15.93c0.02,2.35 -1.1,4.27 -2.87,5.83 -5.92,-1.4 -10.04,-3.11 -10.51,-5.48 -0.65,-3.29 1.88,-5.23 6.1,-5.24 4,-0.01 7.26,1.49 7.28,4.88z"/>
+ </g>
+</svg>
+</template>
